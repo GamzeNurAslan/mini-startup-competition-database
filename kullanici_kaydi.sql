30fb887f700d35c982a9c95097907fc98e0133f0
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-12-27 00:21:56 +0300

    script_1

---
+++ kullanici_kaydi.sql
@@ -0,0 +1,5 @@
+--FR-01 – Kullanýcý Kaydý 
+INSERT INTO dbo.Kullanicilar(ad_soyad, email, sifre, rol, telefon)
+VALUES ('Gamze Nur Aslan', 'gmzorg@mail.com', '123', 'ORGANIZATOR', '5000000000'),
+       ('Büþra Yaþar', 'bsrorg.gmail.com', '456', 'TEMSILCI', '5000000001'),
+       ('Rua Melhem', 'ruaorg.gmail.com', '789', 'JURI', '5000000002');
