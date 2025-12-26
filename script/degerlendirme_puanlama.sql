831ffd3d96304e4e4631c3d2f23302ea5498500f
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-12-27 00:27:03 +0300

    Add files via upload

---
+++ script/degerlendirme_puanlama.sql
@@ -0,0 +1,10 @@
+--FR-07 – Deðerlendirme Puanlama
+EXEC dbo.sp_PuanVer
+ @juri_id = 1,
+ @etkinlik_id = 1,
+ @startup_id = 1,
+ @yenilikcilik = 9,
+ @is_modeli = 8,
+ @teknoloji = 9,
+ @sunum = 8,
+ @yorum = N'Genel olarak iyi';
