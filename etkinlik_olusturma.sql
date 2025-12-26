90f120db35b2951d25c185708b52cdd9aeae98ed
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-12-27 00:24:07 +0300

    script/etkinlik_olusturma.sql

--- etkinlik_olusturma.sql
+++
@@ -1,4 +0,0 @@
---FR-02 – Etkinlik Oluþturma
-INSERT INTO dbo.Yarisma_Etkinlikleri
-(ad, baslangic_tarihi, bitis_tarihi, konum, aciklama, FK_olusturan_kullanici_id)
-VALUES ('Mini Startup 2025', '2025-12-20', '2025-12-25', 'Istanbul', 'Deneme etkinligi', 1);
---
+++ script/etkinlik_olusturma.sql
@@ -0,0 +1,4 @@
+--FR-02 – Etkinlik Oluşturma
+INSERT INTO dbo.Yarisma_Etkinlikleri
+(ad, baslangic_tarihi, bitis_tarihi, konum, aciklama, FK_olusturan_kullanici_id)
+VALUES ('Mini Startup 2025', '2025-12-20', '2025-12-25', 'Istanbul', 'Deneme etkinligi', 1);
