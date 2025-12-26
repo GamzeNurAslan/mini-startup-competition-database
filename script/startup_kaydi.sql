c1a6186f841701950b0138800975f8f108135915
Gamze Nur Aslan <aslangamzenur079@gmail.com>
2025-12-27 00:24:58 +0300

    script/startup_kaydi.sql

---
+++ script/startup_kaydi.sql
@@ -0,0 +1,4 @@
+--FR-03 – Startup Kaydý
+INSERT INTO dbo.Startuplar
+(ad, kategori, aciklama, web_site, FK_temsilci_kullanici_id, FK_etkinlik_id)
+VALUES ('GreenAI', 'Afet Teknolojileri', 'Afet icin cozum', 'https://a.com', 4, 1);
--- startup_kaydi.sql
+++
@@ -1,4 +0,0 @@
---FR-03 – Startup Kaydý
-INSERT INTO dbo.Startuplar
-(ad, kategori, aciklama, web_site, FK_temsilci_kullanici_id, FK_etkinlik_id)
-VALUES ('GreenAI', 'Afet Teknolojileri', 'Afet icin cozum', 'https://a.com', 4, 1);
